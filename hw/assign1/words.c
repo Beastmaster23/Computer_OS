4484e9703f350111d01a5bf9fc0c6f4fe148c32a
Interco <edn1211@gmail.com>
2020-02-15 22:05:50 -0800

    Updated

--- hw/assign1/words.c
+++ hw/assign1/words.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+/*
+ * Function: ReadLineFile
+ * Usage: line = ReadLineFile(File_pointer);
+ * -------------------------------------------------
+ * ReadLineFile Reads a line from a file. One character at time.
+ */
 char *ReadLineFile(FILE *infile)
 {
     int bufferSize = 1;
@@ -26,11 +31,22 @@ char *ReadLineFile(FILE *infile)
     return buffer;
 }
 
+/*
+ * Function: ReadLine
+ * Usage: line = ReadLine(File_pointer);
+ * -------------------------------------------------
+ * ReadLine Reads a line from a standard input. One character at time.
+ */
 char *ReadLine(void)
 {
     return ReadLineFile(stdin);
 }
-
+/*
+ * Function: count_words_in_line
+ * Usage: integer = count_words_in_line(line);
+ * -------------------------------------------------
+ * count_words_in_line Counts words from a line.
+ */
 static int count_words_in_line(char *line)
 {
     int number_of_words = 0;
@@ -44,7 +60,12 @@ static int count_words_in_line(char *line)
     }
     return number_of_words;
 }
-
+/*
+ * Function: count_words_in_file
+ * Usage: integer = count_words_in_file(File_name);
+ * -------------------------------------------------
+ * count_words_in_file Counts words from a file. Seperates them by lines
+ */
 int count_words_in_file(char *filename)
 {
     int number_of_words = 0;
@@ -58,7 +79,12 @@ int count_words_in_file(char *filename)
     fclose(file);
     return number_of_words;
 }
-
+/*
+ * Function: count_words
+ * Usage: integer = count_words(string);
+ * -------------------------------------------------
+ * count_words Counts words from a string. Seperates them by lines
+ */
 int count_words(char *str)
 {
     int number_of_words = 0;
