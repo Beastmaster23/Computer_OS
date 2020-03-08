a104fe793046593483210ec71ef50c4525a80fec
Interco <edn1211@gmail.com>
2020-03-08 15:53:08 -0500

    Update

--- quizes/q8/pipes.c
+++ quizes/q8/pipes.c
@@ -6,15 +6,35 @@ int main(int argc, char *argv[])
 {
     int Pipe1[2];
     int Pipe2[2];
-    int ch1pid, ch2pid, numread, numwrite;
+    int ch1pid = -1, ch2pid = -1, numread, numwrite, d = 0;
     char buf;
+    pipe(Pipe1);
+    ch1pid = fork();
 
     if (ch1pid == 0)
     { /* child 1 */
-        execl(argv[1], "prog1", NULL);
+        dup2(Pipe1[1], STDOUT_FILENO);
+        close(Pipe1[0]);
+        close(Pipe1[1]);
+        execl(argv[1], ".", NULL);
     }
+
+    pipe(Pipe2);
+    ch2pid = fork();
     if (ch2pid == 0)
-    { /* child 2 */
-        execl(argv[2], "prog2", NULL);
+    { /* child 1 */
+        dup2(Pipe2[0], STDIN_FILENO);
+        close(Pipe2[0]);
+        close(Pipe2[1]);
+        execl(argv[2], "", NULL);
     }
+    dup2(Pipe1[0], STDIN_FILENO);
+    dup2(Pipe2[1], STDOUT_FILENO);
+    close(Pipe1[0]);
+    close(Pipe1[1]);
+    close(Pipe2[0]);
+    close(Pipe2[1]);
+    buf=read(STDIN_FILENO, &buf, 1);
+    write(STDOUT_FILENO, &buf, 1);
+    close(STDOUT_FILENO);
 }
